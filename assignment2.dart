1383d876d5801dc88a7fb6fcb0a2281d5bc8c128
JUSTUS KITAVI <159685251+justuskitavi@users.noreply.github.com>
2024-03-07 16:51:07 +0300

    Create assignment2.dart

---
+++ assignment2.dart
@@ -0,0 +1,17 @@
+void main()
+{
+  //using string data type to store a name
+  String name="Justus Kitavi";
+  //using int  data type to store integer data type
+  int age =19;
+  //using doble data type to store a decimal number
+  double weight= 50.4;
+  //using a list data type to store a list
+  List buddies =["Ken", "Joy", "Paul", "Tess"];
+  //using a map data type to store a list of my favourite drinks and their prices 
+  Map<String, double> drinks={'stoney':60.00,'predator':70.00, 'malt':50.00};
+
+  //outputing the above info
+  print("My name is $name and I am $age years old. The last time i checked I weighed $weight kilograms. My best friends are $buddies. My favourite drinks and their respective prices are are $drinks.");
+  
+}
